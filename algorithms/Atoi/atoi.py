5e2a0ac6b639e7e400307ca02dcc6bf0fd31714b
ybcc <271380178@qq.com>
2019-09-06 11:25:30 +0800

    implement Atoi

---
+++ algorithms/Atoi/atoi.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: Yi Bin
+@Date: 2019/9/6
+@Source: https://leetcode.com/problems/string-to-integer-atoi/
+@Description:
+
+    Implement atoi which converts a string to an integer.
+
+    Example 1:
+        Input: "42"
+        Output: 42
+
+    Example 2:
+        Input: "   -42"
+        Output: -42
+        Explanation: The first non-whitespace character is '-', which is the minus sign.
+                    Then take as many numerical digits as possible, which gets 42.
+
+    Example 3:
+        Input: "4193 with words"
+        Output: 4193
+        Explanation: Conversion stops at digit '3' as the next character is not a numerical digit.
+
+    Example 4:
+        Input: "words and 987"
+        Output: 0
+        Explanation: The first non-whitespace character is 'w', which is not a numerical
+                     digit or a +/- sign. Therefore no valid conversion could be performed.
+
+    Example 5:
+        Input: "-91283472332"
+        Output: -2147483648
+        Explanation: The number "-91283472332" is out of the range of a 32-bit signed integer.
+                     Therefore INT_MIN (−2^31) is returned.
+"""
+
+
+def atoi(str: str) -> int:
+    str = str.strip()
+    digits = '0123456789'
+    signs = '-+'
+    if not str or str[0] not in signs + digits:
+        return 0
+
+    sign = 1
+    if str[0] in signs:
+        if str[0] == '-':
+            sign = -1
+        str = str[1:]
+
+    res = 0
+    for c in str:
+        if c not in digits:
+            break
+        res = res * 10 + int(c)
+    res *= sign
+
+    if res > (2 ** 31 - 1):
+        return 2 ** 31 - 1
+    elif res < (-2 ** 31):
+        return -2 ** 31
+    else:
+        return res
+
+
+if __name__ == '__main__':
+    test = ['42', '  -42', '4193 with words', 'words and 987', '-91283472332']
+    for _input in test:
+        print(atoi(_input))
