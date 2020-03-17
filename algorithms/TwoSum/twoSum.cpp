494c14dc26cbd3fcc7b3f5d2c66193d5e779ac75
ybcc <271380178@qq.com>
2020-03-17 22:45:59 +0800

    implement TwoSum in C++

---
+++ algorithms/TwoSum/twoSum.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include<unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target){
+    unordered_map<int, int> m;
+    
+    for(int i=0; i<nums.size(); i++){
+        if(m.find(nums[i]) != m.end())
+            return {m[nums[i]], i};
+        
+        m[target - nums[i]] = i;
+    }
+    
+    return {};
+    }
+};
